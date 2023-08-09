a1b29cab3d21e0d575c24b02363775b2d905c337
Thomas Kosciuch <thomas@qwealth.com>
2023-08-09 15:38:45 -0400

    feat/QC-5371-getting-files-from-2-servers

--- cdk_build/env_vars.py
+++ cdk_build/env_vars.py
@@ -1,31 +1,37 @@
 from typing import TypedDict, LiteralString, Required
 from aws_cdk import aws_ssm
 
-
 class DigitCredentials(TypedDict):
-    digit_ftp_ppk: Required[str]
-    digit_ftp_server: Required[str]
+    digit_sftp_ppk: Required[str]
+    digit_sftp_server: Required[str]
+    digit_sftp_private_key_pass: Required[str]
+    digit_sftp_username: Required[str]
     name: Required[str]
-    nbin_ftp_password: Required[str]
-    nbin_ftp_username: Required[str]
+    nbin_sftp_password: Required[str]
+    nbin_sftp_username: Required[str]
     sentry_dsn: Required[str]
 
 DIGIT_FTP_SERVER:LiteralString = 'ca.sftp.d1g1t.com'
-NBIN_FTP_SERVER = 'sftp.corrnet.com'
+NBIN_FTP_SERVER:LiteralString = 'sftp.corrnet.com'
 
 def digit_nbin_ftp_env_vars(self) -> DigitCredentials:
     get_ssm_value = aws_ssm.StringParameter.value_for_string_parameter
     
-    digit_ftp_ppk = get_ssm_value(self, "DIGIT_FTP_PPK")
-    nbin_ftp_password = get_ssm_value(self, "NBIN_FTP_PASSWORD")
-    nbin_ftp_username = get_ssm_value(self, "NBIN_FTP_USERNAME")
+    digit_sftp_ppk = get_ssm_value(self, "DIGIT_FTP_PPK")
+    digit_sftp_private_key_pass = get_ssm_value(self, "DIGIT_SFTP_PRIVATE_KEY_PASS")
+    digit_sftp_username = get_ssm_value(self, 'DIGIT_SFTP_USERNAME')
+    nbin_sftp_password = get_ssm_value(self, "SFTP_NBIN_PASSWORD")
+    nbin_sftp_username = get_ssm_value(self, "SFTP_NBIN_USERNAME")
     sentry_dsn = get_ssm_value(self, "LAMBDA_SENTRY_DSN")
     
     return {
-        "digit_ftp_ppk": digit_ftp_ppk,
-        "digit_ftp_server": DIGIT_FTP_SERVER,
+        "digit_sftp_ppk": digit_sftp_ppk,
+        "digit_sftp_server": DIGIT_FTP_SERVER,
+        "digit_sftp_private_key_pass": digit_sftp_private_key_pass, 
+        "digit_sftp_username": digit_sftp_username,
         "name": self.name,
-        "nbin_ftp_password": nbin_ftp_password,
-        "nbin_ftp_username": nbin_ftp_username,
+        "nbin_sftp_password": nbin_sftp_password,
+        "nbin_sftp_server": NBIN_FTP_SERVER,
+        "nbin_sftp_username": nbin_sftp_username,
         "sentry_dsn": sentry_dsn
     }
